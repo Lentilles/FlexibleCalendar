b23392959fce7d903512de0b34396733b370cc3f
Tyapkin Alexey <Marveleks@gmail.com>
2025-06-27 11:22:38 +0300

    feat added special dates for templated calendar

---
+++ FlexibleCalendar/Models/SpecialDate.cs
@@ -0,0 +1,11 @@
+﻿using FlexibleCalendar.Interfaces;
+using System.Drawing;
+
+namespace FlexibleCalendar.Models;
+
+public class SpecialDate : ISpecialDate
+{
+    public DateOnly Date { get; set; }
+    public IEnumerable<Color> Colors { get; set; } = [];
+    public Color TextColor { get; set; }
+}
